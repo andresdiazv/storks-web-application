7b0b877c05b47eec20000ff8473e2e9ad46c4bbd
andresdiazv <adiaz616@fiu.edu>
2023-03-11 01:01:52 -0500

    updates

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const { initializeApp } = require('firebase/app');
 const admin = require("firebase-admin");
 const express = require("express");
+const serviceAccount = require("./serviceAccountKey.json");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCUPMErBXb1hF_hW-iWeuRBhdoUmfCl-aI",
-  authDomain: "storks-4753a.firebaseapp.com",
-  projectId: "storks-4753a",
-  storageBucket: "storks-4753a.appspot.com",
-  messagingSenderId: "260702009309",
-  appId: "1:260702009309:web:3ab621d4c28e89ed1ea834",
-  measurementId: "G-ZKEHRFVBM4",
-};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://storks-4753a-default-rtdb.firebaseio.com",
+});
 
-const firebase = initializeApp(firebaseConfig);
-    
-const app = express();
+const db = admin.database();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware function to verify ID token and retrieve idToken from the Realtime Database
+function verifyIdToken(req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  admin
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      userCredential.user.getIdToken().then((idToken) => {
+        req.idToken = idToken;
+        next();
+      });
+    })
+    .catch((error) => {
+      res.render("login", { error: error.message });
+    });
+}
 
 app.get("/", (req, res) => {
   res.redirect("/login");
@@ -33,57 +46,73 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  firebase
+  admin
     .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(() => {
-      res.redirect("/dashboard");
+    .createUser({
+      email: email,
+      password: password,
+    })
+    .then((userRecord) => {
+      // Create a reference to the user's data in the database
+      const userRef = db.ref("users/" + userRecord.uid);
+
+      // Save the user's data to the database
+      userRef.set({
+        email: userRecord.email,
+        uid: userRecord.uid,
+        // Add more properties here as needed
+      });
+
+      admin.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        userCredential.user.getIdToken().then((idToken) => {
+          res.redirect(`/dashboard?idToken=${idToken}`);
+        });
+      })
+      .catch((error) => {
+        res.render("login", { error: error.message });
+      });
     })
     .catch((error) => {
       res.render("register", { error: error.message });
     });
 });
 
-app.get("/login", (req, res) => {
-  res.render("login", { error: null });
+app.get('/login', (req, res) => {
+  res.render('login', { error: null }); // pass in null as the default value for error
 });
 
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+app.post("/login", verifyIdToken, (req, res) => {
+  const idToken = req.idToken;
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then(() => {
-      res.redirect("/dashboard");
-    })
-    .catch((error) => {
-      res.render("login", { error: error.message });
-    });
+  res.redirect(`/dashboard?idToken=${idToken}`);
 });
 
 app.get("/dashboard", (req, res) => {
-  const user = firebase.auth().currentUser;
+  const idToken = req.query.idToken;
 
-  if (user) {
-    res.render("dashboard", { user: user });
-  } else {
-    res.redirect("/login");
-  }
-});
-
-app.get("/logout", (req, res) => {
-  firebase
+  admin
     .auth()
-    .signOut()
-    .then(() => {
-      res.redirect("/login");
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      const userRef = db.ref("users/" + decodedToken.uid);
+
+      userRef.once("value", (snapshot) => {
+        const user = snapshot.val();
+
+        if (user) {
+          res.render("dashboard", { user: user });
+        } else {
+          res.redirect("/login");
+        }
+      });
     })
     .catch((error) => {
-      console.log(error.message);
-      res.redirect("/dashboard");
+      res.redirect("/login");
     });
+});
+app.get("/logout", (req, res) => {
+  res.redirect("/login");
 });
 
 app.listen(port, () => {
