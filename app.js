7973c7451b37421da981023130c36cb250127e7f
Andres Diazvictores <adiaz616@fiu.edu>
2023-03-20 18:12:29 -0400

    login register both work

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ admin.initializeApp({
 });
 
 const db = admin.database();
+const ref = db.ref("restricted_access/secret_document");
+ref.once("value", (snapshot) => {
+  console.log(snapshot.val());
+});
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -37,25 +41,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Middleware function to verify ID token and retrieve idToken from the request
-async function verifyIdToken(req, res, next) {
-  const idToken = req.query.idToken;
-
-  try {
-    const decodedToken = await admin.auth().verifyIdToken(idToken);
-    req.uid = decodedToken.uid;
-    next();
-  } catch (error) {
-    res.redirect("/login");
-  }
-}
 
 app.get("/", (req, res) => {
   res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
-  res.render("register", { error: null });
+  res.render("register");
 });
 
 app.post("/register", (req, res) => {
@@ -75,6 +67,7 @@ app.post("/register", (req, res) => {
       // Save the user's data to the database
       userRef.set({
         email: userRecord.email,
+        password: password,
         uid: userRecord.uid,
         // Add more properties here as needed
       });
@@ -93,7 +86,7 @@ app.post("/register", (req, res) => {
       res.render("register", { error: error.message });
     });
 });
-
+// login start
 app.get("/login", (req, res) => {
   let message = req.flash("error")[0]; // retrieve the first error message from the flash array
   res.render("login", { message }); // pass in the error message as a variable to the login template
@@ -103,83 +96,33 @@ app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  // Fetch the user data from the database based on the user's email
-  const userRef = db.ref("users");
-  userRef.orderByChild("email").equalTo(email).once("value", (snapshot) => {
-    const userData = snapshot.val();
-
-    if (userData) {
-      // User exists with the provided email
-      const userId = Object.keys(userData)[0]; // Get the user ID
-
-      // Compare the user's input password with the password in the database
-      admin
-        .auth()
-        .getUser(userId)
-        .then((userRecord) => {
-          const dbPassword = userData[userId].password;
-
-          if (password === dbPassword) {
-            // Passwords match, generate a custom token and redirect to dashboard
-            admin
-              .auth()
-              .createCustomToken(userRecord.uid)
-              .then((customToken) => {
-                res.redirect(`/dashboard?idToken=${customToken}`);
-              })
-              .catch((error) => {
-                req.flash("error", error.message); // store the error message in the flash array
-                res.redirect("/login"); // redirect to the login page
-              });
-          } else {
-            // Passwords don't match, show an error message
-            req.flash("error", "Invalid password"); // store the error message in the flash array
-            res.redirect("/login"); // redirect to the login page
-          }
-        })
-        .catch((error) => {
-          req.flash("error", error.message); // store the error message in the flash array
-          res.redirect("/login"); // redirect to the login page
-        });
-    } else {
-      // User doesn't exist with the provided email
-      req.flash("error", "User doesn't exist"); // store the error message in the flash array
-      res.redirect("/login"); // redirect to the login page
-    }
-  });
-});
-
-app.get("/dashboard", verifyIdToken, (req, res) => {
-  const idToken = req.query.idToken;
-
-  // Verify the user's ID token to authenticate the user
   admin
     .auth()
-    .verifyIdToken(idToken)
-    .then((decodedToken) => {
-      const userRef = db.ref("users/" + decodedToken.uid);
-      userRef.once("value", (snapshot) => {
-        const user = snapshot.val();
-        res.render("dashboard", { user });
-      });
+    .getUserByEmail(email)
+    .then((userRecord) => {
+      admin
+        .auth()
+        .createCustomToken(userRecord.uid)
+        .then((customToken) => {
+          res.redirect(`/dashboard?token=${customToken}`);
+        })
+        .catch((error) => {
+          res.render("login", { error: error.message });
+        });
     })
-    .catch((error) => {
-      console.error("Error verifying ID token:", error);
-      res.redirect("/login");
-    });
 });
 
-app.get("/data", verifyIdToken, (req, res) => {
-  const dataRef = db.ref("data");
-  dataRef.once("value", (snapshot) => {
-    const data = snapshot.val();
-    res.json(data);
-  });
+// login end
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
 
+//logout
 app.get("/logout", (req, res) => {
   res.redirect("/login");
 });
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
