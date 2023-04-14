81cfbec21ce4ecd13293c337ae22722048a22d2f
pdechario <persephone.dechario@gmail.com>
2023-04-14 14:36:42 -0400

    POST data to favors folder.

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const session = require("express-session");
 const multer = require("multer");
 const app = express();
 const bcrypt = require("bcrypt");
-const { Storage } = require('@google-cloud/storage');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 const saltRounds = 10;
-//const functions = require("./functions");
 
 const path = require("path");
 
@@ -24,13 +24,7 @@ admin.initializeApp({
   storageBucket: "storks-4753a.appspot.com",
 });
 
-const storage = new Storage({
-  projectId: "storks-4753a",
-  keyFilename: "./serviceAccountKey.json",
-});
-
 const db = admin.database();
-
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -61,27 +55,45 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
-  var registerUser = registerUser(email, password, email);
-  if (registerUser == 1) {
-    res.status(500).send("Error hashing password");
-  }
-  else {
 
-  }
+  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+    if (err) {
+      console.error("Error hashing password:", err);
+      res.status(500).send("Error hashing password");
+      return;
+    }
 
-  admin
-    .auth()
-    .createCustomToken(userRecord.uid)
-    .then((customToken) => {
-      res.redirect(`/dashboard?token=${customToken}`);
-    })
-    .catch((error) => {
-      res.render("login", { error: error.message });
-    });
-})
-  .patch((error) => {
-    res.render("register", { error: error.message });
+    admin
+      .auth()
+      .createUser({
+        email: email,
+        password: password,
+      })
+      .then((userRecord) => {
+        const userRef = db.ref("users/" + userRecord.uid);
+
+        userRef.set({
+          name: name,
+          email: userRecord.email,
+          password: hashedPassword,
+          uid: userRecord.uid,
+        });
+
+        admin
+          .auth()
+          .createCustomToken(userRecord.uid)
+          .then((customToken) => {
+            res.redirect(`/dashboard?token=${customToken}`);
+          })
+          .catch((error) => {
+            res.render("login", { error: error.message });
+          });
+      })
+      .catch((error) => {
+        res.render("register", { error: error.message });
+      });
   });
+});
 
 app.get("/login", (req, res) => {
   let message = req.flash("error")[0];
@@ -110,7 +122,7 @@ app.get("/dashboard", (req, res) => {
       .once("value")
       .then((snapshot) => {
         const userData = snapshot.val();
-        res.render("dashboard", { user: userData });
+        res.render("dashboard", { user: userData, db: db });
       })
       .catch((error) => {
         console.error("Error retrieving user data:", error);
@@ -143,10 +155,40 @@ app.get("/profile-page", checkAuthenticated, (req, res) => {
     });
 });
 
+app.get("/favors-page", (req, res) => {
+  res.render("favors-page");
+});
 
-app.get('/favors/:userId', async (req, res) => {
-  const favors = await getFavorsByDateAndUser(req.params.userId);
-  res.render('favors.ejs', { favors });
+app.post("/upload", checkAuthenticated, upload.single("image"), (req, res) => {
+  const userId = req.user.uid;
+  const file = req.file;
+  const bucket = admin.storage().bucket();
+  const filename = `profile.jpg`;
+  const fileUpload = bucket.file(`${userId}/${filename}`);
+
+  const stream = fileUpload.createWriteStream({
+    metadata: {
+      contentType: file.mimetype,
+    },
+  });
+
+  stream.on("error", (error) => {
+    console.error("Error uploading file:", error);
+    res.status(500).send("Error uploading file");
+  });
+
+  stream.on("finish", () => {
+    fileUpload.makePublic().then(() => {
+      const imageUrl = `https://storage.googleapis.com/${bucket.name}/${userId}/${filename}`;
+      const userRef = db.ref(`users/${userId}`);
+      userRef.update({
+        profilePicture: imageUrl,
+      });
+      res.redirect("/profile-page");
+    });
+  });
+
+  stream.end(file.buffer);
 });
 
 app.get("/logout", (req, res) => {
@@ -165,123 +207,45 @@ app.post("/logout", function (req, res, next) {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+// percies edits
+// percies edits
+app.post("/marker", checkAuthenticated, (req, res) => {
+  const userId = req.user.uid;
+  const markerTitle = req.body['marker-name'];
+  const markerPhone = req.body['marker-phone'];
+  const markerAddress = req.body['marker-address'];
+  const markerDescription = req.body['marker-description'];
 
-app.get('/create-task', function (req, res) {
-  // Handle the request and render the appropriate view
+  // Create a new favor node in the database
+  const favorsRef = db.ref("favors");
+  const newFavorRef = favorsRef.push();
+  newFavorRef
+    .set({
+      user_requested: userId,
+      name: markerTitle,
+      phoneNumber: markerPhone,
+      address: markerAddress,
+      description: markerDescription,
+    })
+    .then(() => {
+      const markerRef = db.ref(`favors/${newFavorRef.key}`);
+      markerRef.set({
+        user_requested: userId,
+        user_assigned: 0,
+        date_requested: new Date().toJSON(),
+        date_completed: 0,
+        name: markerTitle,
+        phoneNumber: markerPhone,
+        address: markerAddress,
+        description: markerDescription,
+      });
+      res.redirect("/dashboard");
+    })
+    .catch((error) => {
+      console.error("Error creating task:", error);
+      res.status(500).send("Error creating task");
+    });
 });
-
 app.get("/favor-popup1", (req, res) => {
   res.render("favor-popup1");
 });
-
-
-
-// temp
-function registerUser(email, password, name){
-  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
-      if (err) {
-          console.error("Error hashing password:", err);
-          return 1;
-      }
-      admin
-          .auth()
-          .createUser({
-              email: email,
-              password: password,
-          })
-          .then((userRecord) => {
-              const userRef = db.ref("users/" + userRecord.uid);
-
-              userRef.set({
-                  name: name,
-                  email: userRecord.email,
-                  password: hashedPassword,
-                  uid: userRecord.uid,
-              });
-          })
-          .catch((error) => {
-              console.error("Error creating user:", error);
-          });
-  });
-}
-
-function newFavor(userID, scheduled_date, title, details) {
-    const newFavorRef = db.ref('/favors').push(); // creates a new unique ID for the new favor
-    const newFavorKey = newFavorRef.key; // gets the unique ID of the new favor
-  
-    const newFavorData = {
-      user_requested: userID,
-      user_assigned: null,
-      scheduled_date: scheduled_date,
-      completed_date: null,
-      title: title,
-      details: details,
-      phone: phone,
-      address: address
-    };
-  
-    const updates = {};
-    updates['/favors/' + newFavorKey] = newFavorData; // sets the new favor data at the new ID
-  
-    return db.ref().update(updates); // writes the new favor to the database
-  }
-
-function acceptFavorRequest(db, favorID, userID){
-  db.ref('/favors/' + favorID).set({
-    user_assigned: userID
-  })
-}
-  
-function completeFavor(db, favorID){
-    var today = new Date();
-    db.ref('/favors/' + favorID).set({
-      completed_date: today
-    })
-  }
-    
-  function populateFavorsPage(db, userId){
-    return 0;
-  }
-
-function getDatabaseSnapshot(db, path_to_data) {
-    return new Promise((resolve, reject) => {
-        const databaseRef = db.ref(path_to_data);
-        databaseRef.once('value')
-        .then((snapshot) => {
-            const userData = snapshot.val();
-            resolve(userData);
-        })
-        .catch((error) => {
-            console.error(error);
-            reject(error);
-        });
-    });
-}
-
-  /*
-  to call getDatabaseSnapshot
-  getDatabaseSnapshot(db, '/path/to/data')
-  .then((data) => {
-    // Use the retrieved data
-  })
-  .catch((error) => {
-    // Handle the error
-  });
-  */
-
-async function getFavorsByDateAndUser(userId) {
-    const activeFavors = await Favor.find({
-      user_requested: userId,
-      completed: false
-    }).sort({ scheduled_date: 'asc' });
-  
-    const completedFavors = await Favor.find({
-      user_requested: userId,
-      completed: true
-    }).sort({ completed_date: 'desc' });
-  
-    return {
-      activeFavors,
-      completedFavors
-    };
-  }
